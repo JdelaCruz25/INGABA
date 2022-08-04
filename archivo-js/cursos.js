d749b9182dbcbe20d2fcd11989647daadac2b486
J. de la Cruz <julieta.delacruz@outlook.com>
2022-08-04 00:20:31 -0300

    fetch

--- archivo-js/cursos.js
+++ archivo-js/cursos.js
@@ -1,52 +1,39 @@
 
 /* CURSOS */
 
-class Cursos { 
-    constructor (id, producto, precio, vacantes=10, duracion, horarios, imagen){
-        this.id=id,
-        this.producto=producto,
-        this.precio=precio,
-        this.vacantes=vacantes,
-        this.duracion=duracion,
-        this.horarios=horarios,
-        this.imagen=imagen
-    }
-}
-
-const curso1 = new Cursos (1, "Gastronomía I", 14000, undefined, 8, "Lunes y miércoles de 20 a 22 hs.", "../archivos-multimedia/gastronomia-1.png")
-const curso2 = new Cursos (2, "Gastronomía II", 16000, undefined, 16, "Lunes y miércoles de 18 a 20 hs.", "../archivos-multimedia/gastronomia-2.png")
-const curso3 = new Cursos (3, "Panadería", 18000, undefined, 8, "Miércoles y viernes de 14 a 16 hs.", "../archivos-multimedia/panaderia.png")
-const curso4 = new Cursos (4, "Pastelería I", 20000, undefined, 16, "Martes y jueves de 20 a 22 hs.", "../archivos-multimedia/pasteleria-1.png")
-const curso5 = new Cursos (5, "Pastelería II", 21000, undefined, 18, "Martes y jueves de 18 a 20 hs.", "../archivos-multimedia/pasteleria-2.png")
-const curso6 = new Cursos (6, "Cocina vegana", 20000, undefined, 18, "Martes y viernes de 16 a 18 hs.", "../archivos-multimedia/vegano.png")
-const curso7 = new Cursos (7, "Cocineritos I", 15000, undefined, 8, "Lunes de 14 a 16 hs. y jueves de 16 a 18 hs.", "../archivos-multimedia/cocineritos-1.png")
-const curso8 = new Cursos (8, "Cocineritos II", 17000, undefined, 10, "Martes y jueves de 14 a 16 hs.", "../archivos-multimedia/cocineritos-2.png")
-const curso9 = new Cursos (9, "Cocina para celíacos", 20000, undefined, 16, "Lunes de 16 a 18 hs. y viernes de 18 a 20 hs.", "../archivos-multimedia/menu-ejecutivo.png")
-const curso10= new Cursos (10, "Viandas para menú ejecutivo", 16000, undefined, 8, "Miércoles de 16 a 18 hs. y viernes de 20 a 22 hs.", "../archivos-multimedia/celiacos.png")
-
-
-const cursos = [curso1, curso2, curso3, curso4, curso5, curso6, curso7, curso8, curso9, curso10]
-
 const divCursos=document.getElementById("divCursos")
 
-cursos.forEach(c=> {                 /* id="curso${producto.id}"> */
-    divCursos.innerHTML += `
-    <div class="card border-primary mb-3" style="max-width: 20rem; margin:4px"> 
-        <img src=${c.imagen} class="card-img-top" alt="img">
-        <p>${c.producto}</p>
-        <p>$${c.precio}.-</p>
-        <p>Vacantes: ${c.vacantes}</p>
-        <p>Duración: ${c.duracion}</p>
-        <a class="btn btn-primary" id="botonAgregar">Añadir al carrito</a>
-    </div> 
-    `
+fetch('productos.json')
+.then(promise => promise.json())
+.then (cursos => {
+    cursos.forEach(c=> {                 /* id="curso${producto.id}"> */
+        divCursos.innerHTML += `
+        <div class="card border-primary mb-3" style="max-width: 20rem; margin:4px"> 
+            <img src=${c.imagen} class="card-img-top" alt="img">
+            <p>${c.producto}</p>
+            <p>$${c.precio}.-</p>
+            <p>Vacantes: ${c.vacantes}</p>
+            <p>Duración: ${c.duracion}</p>
+            <a class="btn btn-primary" id="botonAgregar">Añadir al carrito</a>
+        </div> 
+        `
+    })
 })
 
+async function obtenerCursos () {
+    const info = await fetch ('productos.json')
+    const infoConv = await info.json()
+    return infoConv
+}
+
+const arrayCursos = obtenerCursos()
+
+arrayCursos.then (cursos => console.log(data))
 
 
 /* AGREGAR CARRITO */
 
-const carrito = []
+/* const carrito = []
 
 if (localStorage.getItem('storageCarrito')) {
     car=JSON.parse(localStorage.getItem('storageCarrito'))
@@ -75,7 +62,7 @@ botonAgregar.addEventListener('click', () =>{
         `
     })
 })
-
+ */
 
 
 /* MOSTRAR CARRITO */
